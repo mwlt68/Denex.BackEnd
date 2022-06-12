07c5d1df7e7f843b9b1306c1a705ae1a76cb99d7
mwlt68 <mwltgr@gmail.com>
2022-06-12 19:56:21 +0300

    Domain layer add

---
+++ src/Core/Denex.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denex.Domain.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ src/Core/Denex.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using Denex.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denex.Domain.Entities
+{
+    public class Product: BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
