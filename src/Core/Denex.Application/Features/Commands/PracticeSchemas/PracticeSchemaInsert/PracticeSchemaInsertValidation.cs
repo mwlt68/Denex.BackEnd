655c681edd80b1a1ab7dc2a597f606d0b318666a
mwlt68 <mwltgr@gmail.com>
2022-07-02 19:35:09 +0300

    Lesson and subject validation add

---
+++ src/Core/Denex.Application/Features/Commands/PracticeSchemas/PracticeSchemaInsert/PracticeSchemaInsertValidation.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denex.Application.Features.Commands.PracticeSchemas.PracticeSchemaInsert
+{
+    public class PracticeSchemaInsertValidation : AbstractValidator<PracticeSchemaInsertCommand>
+    {
+        public PracticeSchemaInsertValidation()
+        {
+            RuleFor(x => x.Lessons)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Deneme sınavı mutlaka ders içermelidir !");
+            RuleForEach(x => x.Lessons).ChildRules(y =>
+            {
+                y.RuleFor(x => x.QuestionCount)
+                .GreaterThan(0)
+                .WithMessage("Soru sayısı sıfırdan büyük olmalı !");
+                y.RuleFor(x => x.Subjects)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Ders mutlaka konu içermelidir !");
+            });
+
+        }
+    }
+}
---
+++ src/Core/Denex.Application/Features/Commands/PracticeSchemas/PracticeSchemaLessonInsert/PracticeSchemaLessonInsertValidation.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Denex.Application.Features.Commands.PracticeSchemas.PracticeSchemaLessonInsert
+{
+    public class PracticeSchemaLessonInsertValidation : AbstractValidator<PracticeSchemaLessonInsertCommand>
+    {
+        public PracticeSchemaLessonInsertValidation()
+        {
+            RuleFor(x=> x.QuestionCount)
+                .GreaterThan(0)
+                .WithMessage("Soru sayısı sıfırdan büyük olmalı !");
+            RuleFor(x => x.Subjects)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Ders mutlaka konu içermelidir !");
+        }
+    }
+}
