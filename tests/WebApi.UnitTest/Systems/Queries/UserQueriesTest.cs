7049fb675f64a78db547b0db35d1cd304e0fbc28
mwlt68 <mwltgr@gmail.com>
2022-10-30 21:24:47 +0300

    PracticeSchemaQuery GetAll test add.

--- tests/WebApi.UnitTest/Systems/Queries/UserQueriesTest.cs
+++ tests/WebApi.UnitTest/Systems/Queries/UserQueriesTest.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using System.Linq.Expressions;
 using System.Threading;
-using System.Threading.Tasks;
 using AutoMapper;
 using Denex.Application.Dto;
 using Denex.Application.Exceptions;
